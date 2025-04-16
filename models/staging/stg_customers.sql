669e27f0059b14ba5c508561cff07564466bb39a
tomasz.wartak@gmail.com <tomasz.wartak@gmail.com>
2025-04-16 07:02:26 +0000

    Nowe metryki dla stg_orders

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -4,5 +4,7 @@
 SELECT 
     C_CUSTKEY AS customer_id,
 	C_NAME AS customer_name,
+    C_ADDRESS AS customer_address,
+   	C_PHONE AS phone_number,
 	C_NATIONKEY AS country_id
 FROM MY_DWH.STAGING.CUSTOMER_STG
