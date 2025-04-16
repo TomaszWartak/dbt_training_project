b02fb56b4e6706a9a5a5ce1d067cbbce94a8278d
tomasz.wartak@gmail.com <tomasz.wartak@gmail.com>
2025-04-16 08:29:00 +0000

    poprawki błędów

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -6,6 +6,6 @@ SELECT
     O_CUSTKEY AS customer_id,
     O_ORDERDATE AS order_date,
     O_ORDERSTATUS AS order_status,
-    O_ORDERPRIORITY AS order_prioriity
+    O_ORDERPRIORITY AS order_prioriity,
     O_TOTALPRICE AS order_total
 FROM MY_DWH.STAGING.ORDERS_STG
